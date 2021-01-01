254c84df1a39cfcb168300634f64ded08138b4a5
DamonFlick <75501983+DamonFlick@users.noreply.github.com>
2020-12-31 17:33:56 -0800

    Create Project2_functions

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,31 @@
+
+
+function concat() {//Define Variables, assigne string values and change font color
+    var A ="Hello, World!"; 
+    var A = A.fontcolor("green");
+    var B ="\"Oh my!\" said the frog. ";
+    var B = B.fontcolor("orange");
+   
+    document.write(A+=B);
+} //Print joined strings
+
+concat();
+//etc
+var blues= "I have the blues." , blues= blues.fontcolor("blue");
+document.write(blues);
+//Define a variable and give an expression value
+var expression = (3+3);
+document.write(expression); //Print expression
+
+function myFirstFunction() {  //Define function
+    var str = "This text is green!"; //Create variable with string value
+    var result = str.fontcolor("green"); //Change font color
+    document.getElementById("Green_Text").innerHTML = result; //Trun inner HTML to colored string 
+}
+
+
+
+function copyFunc() {
+    document.getElementById("demo").innerHTML = "You copied text!";
+}
+alert("Hello, World!")
