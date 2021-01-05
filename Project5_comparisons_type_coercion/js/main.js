55b78b6db341a7da8e6f2b6ae8227ebbeec7a841
DamonFlick <75501983+DamonFlick@users.noreply.github.com>
2021-01-05 14:25:50 -0800

    Create ternary and constructors project

--- Project5_comparisons_type_coercion/js/main.js
+++ Project5_comparisons_type_coercion/js/main.js
@@ -33,3 +33,19 @@ document.write(10>3 || 2!=3);
 function notFunc() {                 /** Not */
     document.getElementById("Not").innerHTML= !(20>10);
 }
+
+/** Ternairy Operators */
+
+function rideFunc() {
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_ride = (Height > 52) ? "You are too short":"You are tall enough";
+    document.getElementById("ride").innerHTML = Can_ride + "to ride.";
+}
+
+function voteFunc() {
+    var Age, Can_vote;
+    Height = document.getElementById("Age").value;
+    Can_vote = (Age > 18) ? "You're too young ":"You are old enough ";
+    document.getElementById("vote").innerHTML= Can_vote + "to vote."
+}
